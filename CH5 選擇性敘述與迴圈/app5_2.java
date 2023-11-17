2c4d69e4d7a714778a03c9a47213a22e0e7835d4
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-11-17 13:07:52 +0800

    ?:

---
+++ CH5 選擇性敘述與迴圈/app5_2.java
@@ -0,0 +1,9 @@
+public class app5_2{
+    public static void main(String args[]){
+        int a=8, b=3, max;
+        max=(a>b)?a:b;
+
+        System.out.println("a="+a+", b="+b);
+        System.out.println(max+"是較大的數");
+    }
+}
