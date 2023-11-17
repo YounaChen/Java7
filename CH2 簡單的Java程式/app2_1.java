c11e1e32aa1250e886094ea894b885f0989e9253
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-11-17 12:39:36 +0800

    ch2 code

---
+++ CH2 簡單的Java程式/app2_1.java
@@ -0,0 +1,8 @@
+public class app2_1{
+    public static void main(String[] args){
+        int num;
+        num=2;
+        System.out.println("I have " + num +" dogs");
+        System.out.println("You have "+ num +" dogs, too");
+    }
+}
