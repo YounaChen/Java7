b20650fc84e169d072a65fded1bb11698c217148
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-11-17 14:58:12 +0800

    print star & call function

---
+++ CH7 函數/app7_1.java
@@ -0,0 +1,14 @@
+public class app7_1{
+    public static void main(String args[]){
+        star();
+        System.out.println("Knowledge is power");
+        star();
+    }
+
+    public static void star(){ // 印出star的函數
+        for(int i=0;i<20;i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
