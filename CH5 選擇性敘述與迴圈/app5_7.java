4ca68cee5f101d5ca009a7c4753ea4595b538827
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-11-17 13:07:11 +0800

    巢狀迴圈

---
+++ CH5 選擇性敘述與迴圈/app5_7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class app5_7{
+    public static void main(String args[]){
+        int i,j;
+
+        for(i=1;i<=9;i++){
+            for(j=1;j<=9;j++){
+                System.out.print(i+"x"+j+"="+(i*j)+"\t");
+            }
+            System.out.println();
+        }
+
+        
+        
+    }
+}
