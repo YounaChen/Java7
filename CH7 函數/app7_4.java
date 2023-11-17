6ddbfed6f8a533124226131c9cbce8217c500b2b
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-11-17 15:13:05 +0800

    add10( n )

---
+++ CH7 函數/app7_4.java
@@ -0,0 +1,14 @@
+public class app7_4{
+    public static void main(String args[]){
+        int num=5;
+        add10(num);
+        System.out.println("in main(), num="+num);
+        
+    }
+
+    public static void add10(int value){ // 印出star的函數
+        value = value+10;
+
+        System.out.println("in add10(), value="+value);
+    }
+}
